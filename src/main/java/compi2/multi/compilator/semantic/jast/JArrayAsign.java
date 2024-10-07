e3a914b26b0c0d75eeb92a6ddbf3afb10cce09d1
Yennifer017 <yenniferdelion@gmail.com>
2024-10-06 18:02:59 -0600

    java gramar v2

--- src/main/java/compi2/multi/compilator/semantic/jast/JArrayAsign.java
+++ src/main/java/compi2/multi/compilator/semantic/jast/JArrayAsign.java
@@ -1,12 +1,32 @@
 
 package compi2.multi.compilator.semantic.jast;
 
+import compi2.multi.compilator.analysis.symbolt.SymbolTable;
+import compi2.multi.compilator.analysis.typet.TypeTable;
 import compi2.multi.compilator.semantic.Statement;
+import compi2.multi.compilator.semantic.util.ReturnCase;
+import compi2.multi.compilator.semantic.util.SemanticRestrictions;
+import compi2.multi.compilator.util.Position;
+import java.util.List;
 
 /**
  *
  * @author blue-dragon
  */
 public class JArrayAsign extends Statement implements JSimpleStmt{
+
+    public JArrayAsign(Position initPos) {
+        super(initPos);
+    }
+
+    @Override
+    public ReturnCase validate(SymbolTable symbolTable, TypeTable typeTable, List<String> semanticErrors, SemanticRestrictions restrictions) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ReturnCase validateSS(SymbolTable symbolTable, TypeTable typeTable, List<String> semanticErrors, SemanticRestrictions restrictions) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
--- src/main/java/compi2/multi/compilator/semantic/jexp/JOperation.java
+++ src/main/java/compi2/multi/compilator/semantic/jexp/JOperation.java
@@ -1,12 +1,26 @@
 
 package compi2.multi.compilator.semantic.jexp;
 
+import compi2.multi.compilator.analysis.symbolt.SymbolTable;
+import compi2.multi.compilator.analysis.typet.TypeTable;
 import compi2.multi.compilator.semantic.Expression;
+import compi2.multi.compilator.semantic.util.Label;
+import java.util.List;
 
 /**
  *
  * @author blue-dragon
  */
 public class JOperation extends Expression{
+
+    @Override
+    public Label validateSimpleData(SymbolTable symbolTable, List<String> semanticErrors) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Label validateComplexData(SymbolTable symbolTable, TypeTable typeTable, List<String> semanticErrors) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
